e948bcb1fa88c1125d8bdb2031691c7f4212e018
Hugo Gonzalez <pinelo93@gmail.com>
2017-03-07 15:00:03 -0600

    Adds to user crudness

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
+  delete '/sign_out', to: 'sessions#log_out', as: 'sign_out'
   
   resources :events, :event_info, :event_dates
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :edit, :update, :destroy]
+  devise_for :users, controllers: { registrations: 'registrations' }
 end
