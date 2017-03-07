d634fe95a8fe2e9bc0eeadee6818f164f3119c45
Hugo Gonzalez <pinelo93@gmail.com>
2017-03-07 17:16:30 -0600

    Updates for Sprint Report

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   has_many :event_info, dependent: :destroy
   has_many :event_dates, dependent: :destroy
   has_many :registrations, through: :event_dates, dependent: :destroy
+  after_create :create_date
 
   def next_date
   	next_event_date.date
@@ -9,6 +10,13 @@ class Event < ApplicationRecord
 
   def next_event_date
   	event_dates.order('date desc').last
+  end
+
+  def create_date
+  	edate = EventDate.new
+  	edate.event_id = self.id
+  	edate.date = Time.zone.now
+  	edate.save
   end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  describe ".next_event"
-  	xit "brings the next event"
-	end
 end
