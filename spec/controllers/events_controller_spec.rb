630fa6c4bbc3d21a71bd2d3845cbb1b172c40d11
Hugo Gonzalez <pinelo93@gmail.com>
2017-03-06 22:08:50 -0600

    Adds Crud Shared Examples

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,23 +3,4 @@ require 'rails_helper'
 RSpec.describe EventsController, type: :controller do
 	let(:model) {:event}
 	it_behaves_like 'a crud controller'
-
-	# it "should have a current_user" do
-	#   expect(subject.current_user).to_not eq(nil)
-	# end
-
-	# describe 'GET #index' do
-	# 	before do
-	# 		@instances = FactoryGirl.create_list(:event, 3)
-	# 		get :index
-	# 		assert_response :success
-	# 	end
-
-	# 	it "should return all instances" do
-	# 		expect(assigns[:objects]).to eq(@instances)
-	# 	end
-	# end
-
-
-
 end
--- spec/support/crud_controller.rb
+++ spec/support/crud_controller.rb
@@ -9,7 +9,7 @@ RSpec.shared_examples "a crud controller" do
 	  expect(subject.current_user).to_not eq(nil)
 	end
 
-	describe 'GET #index' do
+	describe 'CRUD GET #index' do
 		before do
 			@instances = FactoryGirl.create_list(model, 3)
 			get :index
@@ -21,28 +21,70 @@ RSpec.shared_examples "a crud controller" do
 		end
 	end
 
-	# describe 'GET #new' do
-	# 	before do 
-	# 		get :new
-	# 	end
+	describe 'CRUD GET #new' do
+		it "should have blank instance" do
+			expect(klass).to receive(:new)
+			get :new
+		end
+	end
 
-	# 	it "should have blank instance" do
-	# 		expect(assign[model.to_s]).to eq(FactoryGirl.build model)
-	# 	end
-	# end
+	describe 'CRUD POST #create' do
+		before do 
+			@params = FactoryGirl.attributes_for :event
+		end
 
-	# describe 'POST #create' do
-	# end
+		it "should create the object" do 
+			expect(klass).to receive(:create).and_return(:true)
+			post :create, params: { model => @params}
+		end
+	end
 
-	# describe 'GET #show' do
-	# end
+	describe 'CRUD GET #show' do
+		before do 
+			@instance = FactoryGirl.create model
+			get :show, params: { id: @instance.id }
+		end
 
-	# describe 'GET #edit' do
-	# end
+		it "should return the seeked object" do
+			expect(assigns[:object]).to eq(@instance)
+		end
+	end
 
-	# describe 'PATCH #update' do
-	# end
+	describe 'CRUD GET #edit' do
+		before do 
+			@instance = FactoryGirl.create model
+			get :edit, params: { id: @instance.id }
+		end
 
-	# describe 'DELETE #destroy' do
-	# end
+		it "should return the seeked object" do
+			expect(assigns[:object]).to eq(@instance)
+		end
+	end
+
+	describe 'CRUD PATCH #update' do
+		before do 
+			@instance = FactoryGirl.create model
+			@updates = FactoryGirl.attributes_for model
+			patch :update, params: { id: @instance.id, model => @updates }
+			@instance.reload
+		end
+
+		it "should update the object" do
+			matching = @instance.attributes.transform_keys {|x| x.to_sym}.to_a & 
+								 @updates.to_a
+			expect(matching).to eq(@updates.to_a)
+		end
+
+	end
+
+	describe 'CRUD DELETE #destroy' do
+		before do 
+			@instance = FactoryGirl.create model
+			delete :destroy, params: { id: @instance.id }
+		end
+
+		it "should destroy the object" do
+			expect(klass.exists? @instance).to be(false)
+		end
+	end
 end
