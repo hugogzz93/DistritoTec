3eb53de97a53aa07874ef9aa0355d1d1c75d4e19
Hugo Gonzalez <pinelo93@gmail.com>
2017-03-08 18:23:15 -0600

    Extract specs to shared context

--- spec/support/crud_controller.rb
+++ spec/support/crud_controller.rb
@@ -10,82 +10,30 @@ RSpec.shared_examples "a crud controller" do
 	end
 
 	describe 'CRUD GET #index' do
-		before do
-			FactoryGirl.create_list(model, 3)
-			@instances = klass.all
-			get :index
-			assert_response :success
-		end
-		
-		it "should return all instances" do
-			expect(assigns[:objects]).to eq(@instances)
-		end
+		it_behaves_like 'a crud index'
 	end
 
 	describe 'CRUD GET #new' do
-		it "should have blank instance" do
-			expect(klass).to receive(:new)
-			get :new
-		end
+		it_behaves_like 'a crud new'
 	end
 
 	describe 'CRUD POST #create' do
-		before do 
-			@params = FactoryGirl.attributes_for :event
-		end
-
-		it "should create the object" do 
-			expect(klass).to receive(:create).and_return(:true)
-			post :create, params: { model => @params}
-		end
+		it_behaves_like 'a crud create'
 	end
 
 	describe 'CRUD GET #show' do
-		before do 
-			@instance = FactoryGirl.create model
-			get :show, params: { id: @instance.id }
-		end
-
-		it "should return the seeked object" do
-			expect(assigns[:object]).to eq(@instance)
-		end
+		it_behaves_like 'a crud show'
 	end
 
 	describe 'CRUD GET #edit' do
-		before do 
-			@instance = FactoryGirl.create model
-			get :edit, params: { id: @instance.id }
-		end
-
-		it "should return the seeked object" do
-			expect(assigns[:object]).to eq(@instance)
-		end
+		it_behaves_like 'a crud edit'
 	end
 
 	describe 'CRUD PATCH #update' do
-		before do 
-			@instance = FactoryGirl.create model
-			@updates = FactoryGirl.attributes_for model
-			patch :update, params: { id: @instance.id, model => @updates }
-			@instance.reload
-		end
-
-		it "should update the object" do
-			matching = @instance.attributes.transform_keys {|x| x.to_sym}.to_a & 
-								 @updates.to_a
-			expect(matching).to eq(@updates.to_a)
-		end
-
+		it_behaves_like 'a crud update'
 	end
 
 	describe 'CRUD DELETE #destroy' do
-		before do 
-			@instance = FactoryGirl.create model
-			delete :destroy, params: { id: @instance.id }
-		end
-
-		it "should destroy the object" do
-			expect(klass.exists? @instance).to be(false)
-		end
+		it_behaves_like 'a crud destroy'
 	end
 end
---
+++ spec/support/shared_api_contexts.rb
@@ -0,0 +1,82 @@
+# spec/support/shared_api_contexts.rb
+  shared_context 'a crud index' do
+    before do
+      FactoryGirl.create_list(model, 3)
+      @instances = klass.all
+      get :index
+      assert_response :success
+    end
+    
+    it "should return all instances" do
+      expect(assigns[:objects]).to eq(@instances)
+    end
+  end
+
+  shared_context 'a crud new' do
+    it "should have blank instance" do
+      expect(klass).to receive(:new)
+      get :new
+      assert_response :success
+    end
+  end
+
+  shared_context 'a crud create' do
+    before do 
+      @params = FactoryGirl.attributes_for :event
+    end
+
+    it "should create the object" do 
+      expect(klass).to receive(:create).and_return(:true)
+      post :create, params: { model => @params}
+    end
+  end
+
+  shared_context 'a crud show' do
+    before do 
+      @instance = FactoryGirl.create model
+      get :show, params: { id: @instance.id }
+      assert_response :success
+    end
+
+    it "should return the seeked object" do
+      expect(assigns[:object]).to eq(@instance)
+    end
+  end
+
+  shared_context 'a crud edit' do
+    before do 
+      @instance = FactoryGirl.create model
+      get :edit, params: { id: @instance.id }
+      assert_response :success
+    end
+
+    it "should return the seeked object" do
+      expect(assigns[:object]).to eq(@instance)
+    end   
+  end
+  
+  shared_context 'a crud update' do 
+    before do 
+      @instance = FactoryGirl.create model
+      @updates = FactoryGirl.attributes_for model
+      patch :update, params: { id: @instance.id, model => @updates }
+      @instance.reload
+    end
+
+    it "should update the object" do
+      matching = @instance.attributes.transform_keys {|x| x.to_sym}.to_a & 
+                 @updates.to_a
+      expect(matching).to eq(@updates.to_a)
+    end
+  end
+
+  shared_context 'a crud destroy' do 
+    before do 
+      @instance = FactoryGirl.create model
+      delete :destroy, params: { id: @instance.id }
+    end
+
+    it "should destroy the object" do
+      expect(klass.exists? @instance).to be(false)
+    end
+  end
